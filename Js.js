e35cde1c37d52ddb7f1863d88e869496640b5e07
hemasali <hemo3li7@gmail.com>
2018-07-04 09:32:56 +0300

    first push to master

---
+++ Js.js
@@ -0,0 +1,35 @@
+
+
+document.getElementById("defaultOpen").click(); 
+
+function Open( Name) {
+    
+    var i, box, tablinks;
+    box = document.getElementsByClassName("box");
+    for (i = 0; i < box.length; i++) {
+        box[i].style.opacity = 0;
+        box[i].style.display = "none";
+    }
+
+    var el = document.getElementById(Name);
+    fadeIn(el, 400); 
+document.getElementById(Name).style.display = "block";
+	
+}
+
+
+
+function fadeIn(el, time) {
+
+    var last = +new Date();
+    var tick = function() {
+      el.style.opacity = +el.style.opacity + (new Date() - last) / time;
+      last = +new Date();
+  
+      if (+el.style.opacity < 1) {
+        (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
+      }
+    };
+  
+    tick();
+  }
